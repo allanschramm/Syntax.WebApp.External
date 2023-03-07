787f1233c7bec13e039098d00558fa498157dadb
allanschramm <3208077+allanschramm@users.noreply.github.com>
2023-03-06 22:20:37 -0300

    Add new portfolio components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
-
-// MÓDULOS DO MATERIAL
-import { MatToolbarModule } from '@angular/material/toolbar'
-import { MatListModule } from '@angular/material/list'
-import { MatIconModule } from '@angular/material/icon'
-
-// MÓDULOS DO PROJETO
+// MÓDULOS PADRÕES
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http'
+
+// MÓDULOS DO MATERIALS
+
+// MÓDULOS DO PROJETO
 import { HeaderComponent } from './components/header/header.component';
 import { FinanceComponent } from './components/finance/finance.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { AboutComponent } from './components/about/about.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/portfolio/dashboard/dashboard.component';
+import { TransactionComponent } from './components/portfolio/transaction/transaction.component';
+import { PortfolioSidebarComponent } from './components/portfolio/portfolio-sidebar/portfolio-sidebar.component';
 
 @NgModule({
   declarations: [
@@ -25,18 +27,17 @@ import { FooterComponent } from './components/footer/footer.component';
     FinanceComponent,
     PortfolioComponent,
     AboutComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    DashboardComponent,
+    TransactionComponent,
+    PortfolioSidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-
-    // MATERIAL IMPORTS
-    MatToolbarModule,
-    MatListModule,
-    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/portfolio/portfolio-sidebar/portfolio-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-sidebar',
+  templateUrl: './portfolio-sidebar.component.html',
+  styleUrls: ['./portfolio-sidebar.component.css']
+})
+export class PortfolioSidebarComponent {
+
+}
