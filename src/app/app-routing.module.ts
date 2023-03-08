de6e1d6c723ff8841893bc0006b2237ddf4c58f9
allanschramm <3208077+allanschramm@users.noreply.github.com>
2023-03-07 22:53:11 -0300

    Changed paths from portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { FinanceComponent } from './components/finance/finance.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PortfolioDashboardComponent } from './components/portfolio/portfolio-dashboard/portfolio-dashboard.component';
+import { PortfolioTransactionNewComponent } from './components/portfolio/portfolio-transaction-new/portfolio-transaction-new.component';
 import { PortfolioTransactionComponent } from './components/portfolio/portfolio-transaction/portfolio-transaction.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
@@ -12,9 +13,16 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'finances', component: FinanceComponent },
-  { path: 'portfolio', component: PortfolioComponent },
-  { path: 'portfolio/dashboard', component: PortfolioDashboardComponent },
-  { path: 'portfolio/transaction', component: PortfolioTransactionComponent},
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: PortfolioDashboardComponent },
+      { path: 'transaction', component: PortfolioTransactionComponent },
+      { path: 'novo', component: PortfolioTransactionNewComponent},
+    ],
+  },
   { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent },
 ];
