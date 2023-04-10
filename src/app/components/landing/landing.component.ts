a8b62b48268e2a874f7d11f39effcf062da6ded7
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2023-04-10 17:58:56 -0300

    Changed landing page template

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent {
+
+  constructor(private router: Router) { }
+
+  login(){
+    this.router.navigate(['/login/']);
+  }
+
+  signup(){
+    this.router.navigate(['/register']);
+  }
 
 }
