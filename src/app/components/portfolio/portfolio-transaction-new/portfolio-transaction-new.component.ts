8bad03fc58e3512c2cf4f82e6be1f4be62b94cc4
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2023-04-10 18:00:45 -0300

    Changed the form to use select element

--- src/app/components/portfolio/portfolio-transaction-new/portfolio-transaction-new.component.ts
+++ src/app/components/portfolio/portfolio-transaction-new/portfolio-transaction-new.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Asset } from 'src/app/models/asset';
 import { AssetPortfolio } from 'src/app/models/asset-portfolio';
+import { Portfolio } from 'src/app/models/portfolio';
 import { SyntaxService } from 'src/app/services/syntax.service';
-
 
 @Component({
   selector: 'app-portfolio-transaction-new',
@@ -12,21 +13,40 @@ import { SyntaxService } from 'src/app/services/syntax.service';
 })
 export class PortfolioTransactionNewComponent implements OnInit {
   
-  assetPortfolioForm: FormGroup;
+  assetPortfolioForm!: FormGroup;
+  assetList: Asset[] = []; // Array para armazenar os ativos vindos da API
+  portfolioList: Portfolio[] = []; // Array para armazenar as carteiras vindas da API
   
-  constructor(private formBuilder: FormBuilder, private syntaxService: SyntaxService, private router: Router) {
+  constructor(private formBuilder: FormBuilder, private syntaxService: SyntaxService, private router: Router) {}
+  
+  ngOnInit(): void {
     this.assetPortfolioForm = this.formBuilder.group({
-      quantity: ['', Validators.required],
-      purchasePrice: ['', Validators.required],
-      date: ['', Validators.required],
-      type: ['', Validators.required],
-      idPortfolio: ['', Validators.required],
-      idAsset: ['', Validators.required]
+      idAsset: [''],
+      quantity: [''],
+      purchasePrice: [''],
+      type: [''],
+      date: [''],
+      idPortfolio: ['']
     });
-   }
-  
-  ngOnInit() {
-    
+
+    // Chame os métodos para obter a lista de ativos e carteiras da API
+    this.syntaxService.getAssetList().subscribe(
+      (assets: Asset[]) => {
+        this.assetList = assets;
+      },
+      (error: any) => {
+        console.error('Erro ao obter a lista de ativos:', error);
+      }
+    );
+
+    this.syntaxService.getPortfolioList().subscribe(
+      (portfolios: Portfolio[]) => {
+        this.portfolioList = portfolios;
+      },
+      (error: any) => {
+        console.error('Erro ao obter a lista de carteiras:', error);
+      }
+    );
   }
 
   onSubmit() {
