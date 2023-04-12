8bcdef36f37a182cc6b3b3963cbc6b305c0ddd25
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2023-04-11 21:16:19 -0300

    Added register page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { error } from 'jquery';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordCheck: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  register(): void {
+    if (this.registerForm.valid) {
+      const name = this.registerForm.get('name')?.value;
+      const lastname = this.registerForm.get('lastname')?.value;
+      const email = this.registerForm.get('email')?.value;
+      const password = this.registerForm.get('password')?.value;
+      const passwordCheck = this.registerForm.get('passwordCheck')?.value;
+      
+      this.authService.register(name, lastname, email, password, passwordCheck).subscribe(
+        response => {
+          console.log('Registro bem-sucedido:', response);          
+        },
+        error => {
+          console.log('Erro no registro:', error);          
+        }
+      )
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+  login(){
+    this.router.navigate(['/login/']);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { last, tap } from 'rxjs/operators';
 import { environment } from 'src/environment/environment'
 
 @Injectable({
@@ -27,5 +27,19 @@ export class AuthService {
 
   loginWithLinkedIn(): Observable<void> {
     return this.http.get<void>('/api/Account/LoginWithLinkedIn');
+  }
+
+  register(name: string, lastname: string, email: string, password: string, passwordCheck: string): Observable<any> {
+    const data = {
+      Nome: name,
+      Sobrenome: lastname,
+      Email: email,
+      Password: password,
+      PasswordCheck: passwordCheck
+    };
+    return this.http.post<any>(`${this.apiUrl}/User/register`, data).pipe(
+      // Opcional: você pode adicionar lógica adicional após o registro bem-sucedido, como redirecionar para a página de login, exibir uma mensagem de sucesso, etc.
+      tap(response => console.log('Registro bem-sucedido:', response))
+    );
   }
 }
