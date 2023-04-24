8b3abda6b5ca57b78d940ac58406eb5888c6ca48
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2023-04-24 16:13:28 -0300

    Added Token expiration date

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -1,19 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthInterceptorService implements HttpInterceptor  {
+export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private jwtHelper: JwtHelperService) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.getToken();
 
     if (token) {
+      const decodedToken = this.jwtHelper.decodeToken(token);
+      const expirationDate = new Date(decodedToken.exp * 1000); // Converter para milissegundos
+      const now = new Date();
+
+      // Verificar se o token expirou
+      if (now > expirationDate) {
+        // Remover o token expirado do localstorage
+        this.authService.logout();
+        // Redirecionar o usuário para a página de login
+        window.location.href = '/login';
+        return throwError('Token expirado');
+      }
+
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
@@ -23,5 +37,4 @@ export class AuthInterceptorService implements HttpInterceptor  {
 
     return next.handle(request);
   }
-
 }
