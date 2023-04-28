aa3bf44ee73b22408f03cf88332335134275dedd
Allan Schramm <3208077+allanschramm@users.noreply.github.com>
2023-04-28 20:06:34 -0300

    Fixed properties exhibition on lists

--- src/app/components/finance/finance-transaction-list/finance-transaction-list.component.ts
+++ src/app/components/finance/finance-transaction-list/finance-transaction-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Transaction } from 'src/app/models/transaction';
+import { TransactionClass } from 'src/app/models/transaction-class';
 import { SyntaxService } from 'src/app/services/syntax.service';
 
 @Component({
@@ -10,12 +11,22 @@ import { SyntaxService } from 'src/app/services/syntax.service';
 })
 export class FinanceTransactionListComponent implements OnInit {
   transactionList$!: Observable<Transaction[]>;
+  transactionClassList$!: TransactionClass[];
   p: number = 1;
 
-  constructor(private transactionService: SyntaxService) { }
+  constructor(private syntaxService: SyntaxService) { }
   
   ngOnInit(): void {
-    this.transactionList$ = this.transactionService.getTransactionList();
+    this.transactionList$ = this.syntaxService.getTransactionList();
+    
+    this.syntaxService.getTransactionClassList().subscribe(transactionClassList => {
+      this.transactionClassList$ = transactionClassList;
+    });
   }
   
+  getCategoryName(idClass: number): string {
+    const category = this.transactionClassList$.find(c => c.id === idClass);
+    return category ? category.name : '';
+  }
+
 }
--- src/app/components/portfolio/portfolio-transaction-list/portfolio-transaction-list.component.ts
+++ src/app/components/portfolio/portfolio-transaction-list/portfolio-transaction-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Asset } from 'src/app/models/asset';
+import { AssetClass } from 'src/app/models/asset-class';
 import { AssetPortfolio } from 'src/app/models/asset-portfolio';
+import { Portfolio } from 'src/app/models/portfolio';
 import { SyntaxService } from 'src/app/services/syntax.service';
 
 @Component({
@@ -10,11 +13,45 @@ import { SyntaxService } from 'src/app/services/syntax.service';
 })
 export class PortfolioTransactionListComponent implements OnInit {
   assetPortfolioList$!: Observable<AssetPortfolio[]>;
+  assetList$!: Asset[];
+  assetClassList$!: AssetClass[];
+  portfolioList$!: Portfolio[];
   p: number = 1;
-    
-  constructor(private assetPortfolioService: SyntaxService) { }
+  
+  constructor(private syntaxService: SyntaxService) { }
 
   ngOnInit(): void {
-    this.assetPortfolioList$ = this.assetPortfolioService.getAssetPortfolioList();
-  }  
+    this.assetPortfolioList$ = this.syntaxService.getAssetPortfolioList();
+
+    this.syntaxService.getAssetList().subscribe(assetList => {
+      this.assetList$ = assetList;  
+    });
+
+    this.syntaxService.getAssetClassList().subscribe(assetClassList => {
+      this.assetClassList$ = assetClassList;  
+    });
+
+    this.syntaxService.getPortfolioList().subscribe(portfolioList => {
+      this.portfolioList$ = portfolioList;
+    });
+  }
+
+  getAssetName(idAsset: number): string {
+    const category = this.assetList$.find(c => c.id === idAsset);
+    return category ? category.name : '';
+  }
+
+  getAssetClassName(idAsset: number): string {
+    const asset = this.assetList$.find(a => a.id === idAsset);
+    if (asset) {
+      const assetClass = this.assetClassList$.find(c => c.id === asset.idAssetClass);
+      return assetClass ? assetClass.name : '';
+    }
+    return '';
+  }
+
+  getPortfolioName(idPortfolio: number): string {
+    const category = this.portfolioList$.find(c => c.id === idPortfolio);
+    return category ? category.name : '';
+  }
 }
